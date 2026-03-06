49c7338b6ec28420518635a9972d7cf44123a955
thinkbig030319-commit <thinkbig030319-commit@gmail.com>
2026-03-06 13:11:31 +0530

    Patch commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  const isAuth = localStorage.getItem("token");
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={isAuth ? <Dashboard /> : <Navigate to="/" />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
